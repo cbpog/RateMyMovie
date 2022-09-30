15070ee1a352109de696d7b3b6fdf2a655c1c10b
cbpog <bcorey395@gmail.com>
2022-09-30 13:11:26 -0400

    Resolved errors created by unimplemented methods. Removed unnecessary methods from class.

--- lib/movie.dart
+++ lib/movie.dart
@@ -1,6 +1,4 @@
-import 'package:tmdb_api/tmdb_api.dart';
-
-class Movies implements {
+class Movies {
   final bool isAdultContent;
   final List genres;
   final int movieId;
@@ -8,26 +6,16 @@ class Movies implements {
   final String movieDescription;
   final String releaseDate;
   final String posterPath;
-  List movies = [];
+  final List movies;
 
-  Movies(
-      {
-        required this.isAdultContent,
-      required this.genres,
-      required this.movieId,
-      required this.movieDescription,
-      required this.movieTitle,
-      required this.releaseDate,
-      required this.posterPath,
-      required this.movies,
-
-      });
-
-  loadMovies() async {
-    TMDB movieInfo = TMDB(ApiKeys(apiKey, apiToken),
-        logConfig: const ConfigLogger(showLogs: true, showErrorLogs: true));
-    Map trendingResults = await movieInfo.v3.trending.getTrending();
-    movies = trendingResults['results'];
-    print(trendingMovies);
-  }
+  Movies({
+    required this.isAdultContent,
+    required this.genres,
+    required this.movieId,
+    required this.movieDescription,
+    required this.movieTitle,
+    required this.releaseDate,
+    required this.posterPath,
+    required this.movies,
+  });
 }
