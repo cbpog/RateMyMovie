7a85c44db74db79a0c42a8d297e5f941f6e2b087
Kevin <kai38547@gmail.com>
2022-10-05 12:02:30 -0400

    Test 'I can pull a movie title from the api' passed

--- test/movie_extract_test.dart
+++ test/movie_extract_test.dart
@@ -4,7 +4,8 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('I can pull a movie title from the api', () async {
-    final answer = await ApiRequest().fetchMovies();
+    final list = await ApiRequest().fetchMovies();
+    final answer = list.first.movieTitle;
     expect(answer, 'Orphan: First Kill');
   });
 }
