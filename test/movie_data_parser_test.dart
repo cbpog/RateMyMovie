2607565b40394c1b0078dfe45e8d1d6407133d25
NMMAY1234 <nmattmay@gmail.com>
2022-10-14 15:14:03 -0400

    Finished parsing movie card data.

--- test/movie_data_parser_test.dart
+++ test/movie_data_parser_test.dart
@@ -18,10 +18,47 @@ void main() {
     expect(answer, '2022-08-11');
   });
 
-  test('I can get movie genre', () async {
+  test('I can get a movie genre', () async {
     final json = await File('test/movie_beast.json').readAsString();
     final jsonData = jsonDecode(json);
-    final answer = jsonData['genres'][0]['id'];
-    expect(answer, 12);
+    final answer = jsonData['genres'][0]['name'];
+    expect(answer, 'Adventure');
+  });
+
+  test('I can get all of the movie genres', () async {
+    final json = await File('test/movie_beast.json').readAsString();
+    final jsonData = jsonDecode(json);
+    final genreList = [];
+    var genreData = jsonData['genres'] as List;
+    for (var i = 0; i < genreData.length; i++) {
+      genreList.add(genreData[i]['name']);
+    }
+    expect(genreList, ['Adventure', 'Drama', 'Horror']);
+  });
+
+  test('I can get the movie overview', () async {
+    final json = await File('test/movie_beast.json').readAsString();
+    final jsonData = jsonDecode(json);
+    final answer = jsonData['overview'];
+    expect(
+        answer,
+        "A recently widowed man and his two teenage daughters " +
+            "travel to a game reserve in South Africa. However, their journey of " +
+            "healing soon turns into a fight for survival when a bloodthirsty lion " +
+            "starts to stalk them.");
+  });
+
+  test('I can get the movie id', () async {
+    final json = await File('test/movie_beast.json').readAsString();
+    final jsonData = jsonDecode(json);
+    final answer = jsonData['id'];
+    expect(answer.toString(), '760741');
+  });
+
+  test('I can get the poster path', () async {
+    final json = await File('test/movie_beast.json').readAsString();
+    final jsonData = jsonDecode(json);
+    final answer = jsonData['poster_path'];
+    expect(answer, "/xIGr7UHsKf0URWmyyd5qFMAq4d8.jpg");
   });
 }
